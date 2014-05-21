115b927946f113a8237978fa81366d7b56e2fbc6
N.Shimizu <chikoski@gmail.com>
2014-05-21 20:38:11 +0900

    平均値を求めるプログラムを作成

---
+++ js/stats.js
@@ -0,0 +1,53 @@
+var DATA_SIZE = 5;
+
+var inputElements = [];
+var outputMean = null;
+
+
+/**
+ * initInputElements: データ入力用のテキスト入力集めた配列を作成
+ */
+var initInputElements = function(){
+    var i = 0;
+    while(i < DATA_SIZE){
+        var id = "#number_" + (i + 1);
+        // #input_1, #input_2 のような id を i から作る
+        var elm = document.querySelector(id);
+        inputElements.push(elm); // elm を inputElements に追加
+        i = i + 1;
+    }
+};
+
+/**
+ * calcMean: 入力されたデータから平均値を計算して出力する
+ */
+var calcMean = function(){
+    var index = 0;
+    var sum = 0;
+    var n = 0;
+
+    while(index < inputElements.length){
+        var input = inputElements[index];
+        var number = Number(input.value);
+        if(!Number.isNaN(number)){
+            sum = sum + number;
+            n = n + 1;
+        }
+        index = index + 1;
+    }
+
+    if(n > 0){
+        outputMean.textContent = sum / n;
+    }
+};
+
+var initApp = function(){
+    initInputElements();
+
+    outputMean = document.querySelector("#mean");
+
+    var calcMeanButton = document.querySelector("#calcMean");
+    calcMeanButton.addEventListener("click", calcMean);
+};
+
+initApp();
