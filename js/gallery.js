33bacc9679066b6452f499c315c154aa8c156165
tsutaya <suzukitatsuya@net76-dhcp13.sfc.keio.ac.jp>
2014-05-28 12:25:55 +0900

    画像追加

--- js/gallery.js
+++ js/gallery.js
@@ -6,11 +6,16 @@ var photoList = [
 	  {src: "img/200902.png", title: "9月"},
 	  {src: "img/201008.png", title: "もう1回8月"},
     {src: "img/front.png", title: "前"},
+<<<<<<< HEAD
     {src:	"img/side.png", title: "横"},
     {src:   "img/hiyaasityuuka.png", title: "冷やし中華"},
 
 ];//photoListの中に新たに画像を入れた。フォルダ「img」の中にhiyaasityuuka.pngを入れ
 　//表示させた
+=======
+    {src:	"img/side.png", title: "横"}
+];
+>>>>>>> parent of ad2a5b7... 課題１
 
 var isReady = function(){
     return photoListElement != null &&
